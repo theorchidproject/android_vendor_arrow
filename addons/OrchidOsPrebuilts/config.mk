95945784130375728cde10aa5ac02eb082393ac7
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-02-11 12:50:01 +0000

    OrchidOs Import : Add Coreapps FOSS
    add   audioR pdf   adaway  dns66 io.github.domi04151309.alwayson_383  com.asdoi.quicktiles_14l

--- addons/OrchidOsPrebuilts/config.mk
+++ addons/OrchidOsPrebuilts/config.mk
@@ -5,4 +5,10 @@ PRODUCT_PACKAGES += \
     delta-v1.7.6-foss \
     TapTap-0.2-Alpha \
     rethinkdns25 \
-    com.drnoob.datamonitor_11 
+    com.drnoob.datamonitor_11 \
+    audioR \
+    pdf \
+    adaway \
+    dns66 \
+    io.github.domi04151309.alwayson_383 \
+    com.asdoi.quicktiles_14
